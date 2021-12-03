aeab32727343de1aa3efb6d72c563a67defa0ad8
Jakub Neruda <nerudaj@email.cz>
2021-12-03 10:31:53 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -4,5 +4,3 @@ del dipy-pesta-marinady-recepty.pdf
 del sladke-recepty.pdf
 
 md-to-pdf kucharka.md pecivove-recepty.md dipy-pesta-marinady-recepty.md sladke-recepty.md --stylesheet styles.css --launch-options "{ \"args\" : [\"--no-sandbox\"] }"
-
-move *.pdf pdf/
